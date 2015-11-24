1a02ab14c72ec69fda43e80a88eb90c22d6aacc0
wkharrod <wkharrod@gmail.com>
2015-11-24 15:35:56 -0600

    Step 13

--- testXamarinApp/testXamarinApp/testXamarinApp.cs
+++ testXamarinApp/testXamarinApp/testXamarinApp.cs
@@ -8,18 +8,7 @@ namespace testXamarinApp
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new testXamarinApp.MainPage ();
 		}
 
 		protected override void OnStart ()
