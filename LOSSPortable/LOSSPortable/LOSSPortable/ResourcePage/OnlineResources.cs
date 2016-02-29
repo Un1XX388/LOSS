b2aa5adaf5cab7446c02d4b7735d5f7ae78a55bf
isha soni <isha.soni.18@gmail.com>
2016-02-29 12:15:04 -0600

    basic webview on online resources. text-to-speech still in progress

--- LOSSPortable/LOSSPortable/LOSSPortable/ResourcePage/OnlineResources.cs
+++ LOSSPortable/LOSSPortable/LOSSPortable/ResourcePage/OnlineResources.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using Xamarin.Forms;
 using System.Collections.Generic;
+using Plugin.TextToSpeech;
 
 namespace LOSSPortable{
-	public class OnlineResources : ContentPage{
-		public OnlineResources(){
+    public class OnlineResources : ContentPage {
+        public OnlineResources() {
             //sets the background color based on settings
             if (Helpers.Settings.ContrastSetting == true)
             {
@@ -16,6 +17,52 @@ namespace LOSSPortable{
             }
 
             Title = "Online Resources";
-		}// End of OnlineResources() constructor.
-	}// End of OnlineResources class.
+
+            WebView webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = "http://blog.xamarin.com/",
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            this.Content = webView;
+
+        }// End of OnlineResources() constructor.
+
+
+        //private async void starthttp()
+        //{
+        //    WebRequest request = HttpWebRequest.Create("http://blog.xamarin.com");
+
+        //    WebResponse response = await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, null);
+
+        //    var html = new HtmlDocument();
+        //    html.Load(response.GetResponseStream());
+        //    var nodes = html.DocumentNode.Descendants();
+        //    // The root page of your application
+        //    string result = "";
+        //    foreach (var node in nodes)
+        //    {
+
+        //        result = result + node.OuterHtml;
+
+        //    }
+        //    MainPage = new ContentPage
+        //    {
+        //        Content = new StackLayout
+        //        {
+        //            VerticalOptions = LayoutOptions.Center,
+        //            Children = {
+        //            new Label {
+
+        //                Text = result
+        //            }
+        //        }
+        //        }
+        //    };
+        //}
+
+    }// End of OnlineResources class.
 }// End of namespace LOSS.
