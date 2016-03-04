6259cb4f569b4684bedf4764546ccb4554fdf9d1
idan zoarets <idan.zoarets@mavs.uta.edu>
2016-03-04 15:10:58 -0600

    Re-added the survey, with an icon. Updated the chatSelection page with a toggle to chat.

---
+++ LOSSPortable/LOSSPortable/LOSSPortable/Survey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LOSSPortable
+{
+    public class Survey : ContentPage
+    {
+        public WebView browser;
+
+        public Survey()
+        {
+            browser = new WebView
+            {
+                Source = "https://qtrial2016q1az1.qualtrics.com/SE/?SID=SV_ezirKxBzZdk9Kzr"
+            };
+
+            this.Content = browser;
+        }
+        protected override Boolean OnBackButtonPressed() // back button pressed
+        {
+            ((RootPage)App.Current.MainPage).NavigateTo();
+
+            return true;
+        }
+    }
+}
