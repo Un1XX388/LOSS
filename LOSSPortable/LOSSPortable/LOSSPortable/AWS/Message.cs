77b8746d890f97a48467ed56a5975add51f14015
Green-Apple <matthew.meelhuysen@gmail.com>
2016-03-04 15:39:59 -0600

    Basic lambda functionality enabled, now connects to AWS lambda service and sends/gets responses.

---
+++ LOSSPortable/LOSSPortable/LOSSPortable/AWS/Message.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using Amazon.Lambda.Model;
+
+namespace LOSSPortable
+{
+
+    [DynamoDBTable("Message")]
+    public class ChatMessage
+    {
+        [DynamoDBHashKey("To#From")]
+        public string ToFrom { get; set; }
+
+        [DynamoDBRangeKey("Time")]
+        public string Time { get; set; }
+
+        [DynamoDBProperty("Text")]
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return "To#From:" + ToFrom + ",\nTime:" + Time + ",\nText:" + Text;
+        }
+    }
+}
