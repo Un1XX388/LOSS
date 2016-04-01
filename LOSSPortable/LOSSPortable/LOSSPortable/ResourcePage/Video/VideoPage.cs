e8f5e0f285ecf56926fb5db2776933f799cd559d
Reed Clanton <un1xx388@gmail.com>
2016-04-01 15:09:17 -0500

    Improved look of video on video page by allowing it to take up the whole screen in any rotation. I also prevented the video from going off screen when in landscape.

--- LOSSPortable/LOSSPortable/LOSSPortable/ResourcePage/Video/VideoPage.cs
+++ LOSSPortable/LOSSPortable/LOSSPortable/ResourcePage/Video/VideoPage.cs
@@ -21,7 +21,7 @@ namespace LOSSPortable{
 			// Create variable for holding page as a html string.
 			var htmlSource			= new HtmlWebViewSource();
 			// Compile html string for page.
-			htmlSource.Html			= @create_html("5a3c5c", url);
+			htmlSource.Html			= @create_html(url);
 			// Set the source for this page (the page as a html stirng).
 			vid.Source				= htmlSource;
 			// Set the created html string to be the content of the page.
@@ -29,52 +29,18 @@ namespace LOSSPortable{
 		}// End of VideoPage() constructor.
 
 		// Creates HTML page.
-		private string create_html(string color, string url){
+		private string create_html(string url){
 			// String for holding each element of html page.
-			string[] html_string	= new string[42];
+			string[] html_string	= new string[8];
 
-			html_string[0]			= "<html>";
-			html_string[1]			= "<body bgcolor=\"#";
-			html_string[2]			= color;
-			html_string[3]			= "\">";
-			html_string[4]			= "<div ";
-			html_string[5]			= "style='";
-			html_string[6]			= "position: ";
-			html_string[7]			= "relative;";
-			html_string[8]			= "width: ";
-			html_string[9]			= "100";
-			html_string[10]			= "%;height: ";
-			html_string[11]			= "0";
-			html_string[12]			= "px;padding-bottom: ";
-			html_string[13]			= "60";
-			html_string[14]			= "%;'> ";
-			html_string[15]			= "<iframe ";
-			html_string[16]			= "style='";
-			html_string[17]			= "position: ";
-			html_string[18]			= "absolute";
-			html_string[19]			= ";left: ";
-			html_string[20]			= "0";
-			html_string[21]			= "px;";
-			html_string[22]			= "top: ";
-			html_string[23]			= "0";
-			html_string[24]			= "px;";
-			html_string[25]			= "width: ";
-			html_string[26]			= "100";
-			html_string[27]			= "%;";
-			html_string[28]			= "height: ";
-			html_string[29]			= "100";
-			html_string[30]			= "%' ";
-			html_string[31]			= "src=\"";
-			html_string[32]			= url;
-			html_string[33]			= "\" ";
-			html_string[34]			= "frameborder=\"";
-			html_string[35]			= "0";
-			html_string[36]			= "\" ";
-			html_string[37]			= "allowfullscreen>";
-			html_string[38]			= "</iframe> ";
-			html_string[39]			= "</div>";
-			html_string[40]			= "</body>";
-			html_string[41]			= "</html/>";
+			html_string[0]			= "<html><body bgcolor=\"#";
+			html_string[1]			= "000000";
+			html_string[2]			= "\">";
+			html_string[3]			= "<div style='position:relative; width:100%; height:100%;'>";
+			html_string[4]			= "<iframe style='width:100%; height:100%;' frameborder=0 src=";
+			html_string[5]			= url;
+			html_string[6]			= "></iframe>";
+			html_string[7]			= "</div></body></html>";
 
 			// Cat each element of html page into one string and return.
 			return String.Join("", html_string);
