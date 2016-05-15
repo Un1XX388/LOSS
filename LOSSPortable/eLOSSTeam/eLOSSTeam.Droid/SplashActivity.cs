326e9593e35ec9b61299e02f7e85c6775ebb0352
isha soni <isha.soni.18@gmail.com>
2016-05-15 16:38:06 -0500

    renamed lossportable project. fixed crashing error upon opening the app after clearing data

---
+++ LOSSPortable/eLOSSTeam/eLOSSTeam.Droid/SplashActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+
+namespace eLOSSTeam.Droid
+{
+    [Activity(
+        Label = "eLOSSTeam",
+        Icon = "@drawable/App6",
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize /*| ConfigChanges.Orientation*/,
+        Theme = "@style/Theme.Splash",
+        NoHistory = true           
+        )]
+
+    //this class displays the splash screen
+
+    public class SplashActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            System.Threading.Thread.Sleep(2000);            //displays it for a few seconds before it launches the home screen
+            this.StartActivity(typeof(MainActivity));          
+        }
+
+
+    }
+}
+
---
+++ LOSSPortable/eLOSSTeam/eLOSSTeam/HomePage/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eLOSSTeam
+{
+    public class Location
+    {
+        public string Longitude { get; set; }
+
+        public string Latitude{ get; set; }
+    }// End of Location class.
+}// End of LOSSPortable namespace.
