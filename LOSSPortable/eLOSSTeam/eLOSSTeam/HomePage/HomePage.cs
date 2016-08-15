01c2ce597b255a2a8b761d280b91a95373b87d5f
Green-Apple <matthew.meelhuysen@gmail.com>
2016-08-14 20:04:39 -0500

    Minor changes to HomePage on terminate to add with debugging.

--- LOSSPortable/eLOSSTeam/eLOSSTeam/HomePage/HomePage.cs
+++ LOSSPortable/eLOSSTeam/eLOSSTeam/HomePage/HomePage.cs
@@ -167,7 +167,26 @@ namespace eLOSSTeam
 
         public async Task<bool> IsConnected()
         {
-            return CrossConnectivity.Current.IsConnected && await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            bool isConnected = CrossConnectivity.Current.IsConnected;
+            if (isConnected == true)
+            {
+                var isReachable = await CrossConnectivity.Current.IsRemoteReachable("google.com", 80, 2000);
+                if (isReachable == true)
+                {
+                    Debug.WriteLine("isReachable: true");
+                    return true;
+                }
+                else
+                {
+                    Debug.WriteLine("isReachable: false");
+                    return false;
+                }
+            }
+            else
+            {
+                Debug.WriteLine("isConnected: false");
+                return false;
+            }
         }
 
         //========================================================= FUNCTIONS =========================================================
